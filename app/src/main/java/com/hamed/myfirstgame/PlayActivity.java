0a624cf6eadfc8540a004b9f866dfb3121220f3c
BlueMosquito <61717325+BlueMosquito@users.noreply.github.com>
2022-01-14 12:18:42 +0100

    p2p

--- app/src/main/java/com/hamed/myfirstgame/PlayActivity.java
+++ app/src/main/java/com/hamed/myfirstgame/PlayActivity.java
@@ -2,16 +2,27 @@ package com.hamed.myfirstgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -20,6 +31,13 @@ public class PlayActivity extends AppCompatActivity {
     TextView readMessage;
     EditText writeMessage;
     WifiManager wifiManager;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+    List<WifiP2pDevice> peers = new ArrayList<>();
+    String[] deviceNameArray;
+    WifiP2pDevice[] deviceArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,30 +49,95 @@ public class PlayActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_play);
 
-        btnOnOff = (Button) findViewById(R.id.wifi);
-        btnFindOthers = (Button) findViewById(R.id.findPlayers);
-        btnSendMessage = (Button) findViewById(R.id.send);
-        listView = (ListView) findViewById(R.id.peerListView);
-        readMessage = (TextView) findViewById(R.id.highScore);
-        writeMessage = (EditText) findViewById(R.id.editText);
+        btnOnOff = findViewById(R.id.wifi);
+        btnFindOthers = findViewById(R.id.findPlayers);
+        btnSendMessage = findViewById(R.id.send);
+        listView = findViewById(R.id.peerListView);
+        readMessage = findViewById(R.id.highScore);
+
+        writeMessage = findViewById(R.id.editText);
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        btnOnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (wifiManager.isWifiEnabled()) {
-                    wifiManager.setWifiEnabled(false);
-                } else {
-                    wifiManager.setWifiEnabled(true);
-                }
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        btnOnOff.setOnClickListener(v -> {
+            if (wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(false);
+            } else {
+                wifiManager.setWifiEnabled(true);
             }
         });
 
-        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+        btnFindOthers.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(PlayActivity.this, GameActivity.class));
+            public void onClick(View v) {
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int i) {
+                        Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+
+        findViewById(R.id.play).setOnClickListener(view -> startActivity(new Intent(PlayActivity.this, GameActivity.class)));
     }
+
+    WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+            List<WifiP2pDevice> refreshedPeers = (List<WifiP2pDevice>) peerList.getDeviceList();
+            if (!refreshedPeers.equals(peers)){
+                peers.clear();
+                peers.addAll(refreshedPeers);
+
+                deviceNameArray = new String[peerList.getDeviceList().size()];
+                deviceArray = new WifiP2pDevice[peerList.getDeviceList().size()];
+                int index = 0;
+
+                for (WifiP2pDevice device : peerList.getDeviceList()){
+                    deviceNameArray[index] = device.deviceName;
+                    deviceArray[index] = device;
+                    index++;
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1, deviceNameArray);
+                listView.setAdapter(adapter);
+            }
+
+            if (peers.size() == 0){
+                Toast.makeText(getApplicationContext(), "No Device Found!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    };
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        WiFiDirectBroadcastReceiver receiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+   @Override
+   protected void onPause(){
+        super.onPause();
+        unregisterReceiver(mReceiver);
+   }
 }
